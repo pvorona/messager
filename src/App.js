7f2880397320e624e14238531ca33a3c4c17db4d
Pavel V <vorona.pavel.nickolaevich@gmail.com>
2019-03-02 19:52:31 +0300

    [25-45min]: Styling chat list

--- src/App.js
+++ src/App.js
@@ -32,18 +32,21 @@ const Chat = () => {
   )
 }
 
-const Dialog = ({ id, message }) =>
+const Dialog = ({ userName, message, imgUrl }) =>
   <div className="dialog">
-    {message}
+    <img className="dialog__image" alt="sender avatar" src={imgUrl} />
+    <div className="dialog__content">
+      <div className="dialog__author">{userName}</div>
+      <div className="dialog__message">{message}</div>
+    </div>
   </div>
 
 const DialogList = ({ dialogs }) =>
   <div className="dialog-list">
-    {dialogs.map(({ id, message }) =>
+    {dialogs.map(({ id, ...message }) =>
       <Dialog
-        id={id}
-        message={message}
         key={id}
+        {...message}
       />
     )}
   </div>
@@ -52,16 +55,20 @@ DialogList.defaultProps = {
   dialogs: [{
     id: 1,
     message: 'Hello friend!',
+    userName: 'Robbie Williamson',
+    imgUrl: 'https://randomuser.me/api/portraits/men/49.jpg',
   }, {
     id: 2,
     message: 'That was really nice weekend, we should definitely repeat it on the next week!',
+    userName: 'Robbie Williamson',
+    imgUrl: 'https://randomuser.me/api/portraits/men/49.jpg',
   }],
 }
 
 const App = () =>
-  <>
+  <div className="app">
     <DialogList />
     <Chat />
-  </>
+  </div>
 
 export default App;
